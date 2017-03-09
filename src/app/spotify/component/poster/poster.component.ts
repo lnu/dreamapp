669595b49c2f644c3b6fee51c0b084d38f276744
student <student@bt-training.be>
2017-03-09 16:58:27 +0100

    removed unused code

--- src/app/spotify/component/poster/poster.component.ts
+++ src/app/spotify/component/poster/poster.component.ts
@@ -17,18 +17,6 @@ import * as _ from 'lodash';
       transition('* => void', [
         animate(100, style({ transform: 'translateX(100%)' }))
       ])
-    ]),
-    trigger('heroState', [
-      state('inactive', style({
-        backgroundColor: '#eee',
-        transform: 'scale(1)'
-      })),
-      state('active', style({
-        backgroundColor: '#cfd8dc',
-        transform: 'scale(1.1)'
-      })),
-      transition('inactive => active', animate('100ms ease-in')),
-      transition('active => inactive', animate('100ms ease-out'))
     ])
    ]
 })
